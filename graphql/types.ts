fd2fc10774cbeb59b8d7204f103359629f5e922b
paolaPosada <paola.posada1@udea.edu.co>
2022-03-20 12:34:01 -0500

    correccione de errores base

--- graphql/types.ts
+++ graphql/types.ts
@@ -1,13 +1,8 @@
 import { gql } from 'apollo-server-micro';
-import { VacancyTypes } from 'graphql/models/vacancy/types';
-import { DocumentTypes } from 'graphql/models/document/types';
+import { DocumentTypes } from './models/document/types';
 
 const genericTypes = gql`
   scalar Date
 `;
 
-export const types = [
-  genericTypes,
-  VacancyTypes,
-  DocumentTypes
-];
+export const types = [genericTypes, DocumentTypes];
