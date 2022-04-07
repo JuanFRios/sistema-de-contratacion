b7a0d8417c30e7563fea9638b43d73b2c9a77827
Juan Gutiérrez <juanesguti19@gmail.com>
2022-04-06 20:06:58 -0500

    Small change uri

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: from([
     new HttpLink({
+      // uri: 'https://gestion-proyectos.vercel.app/api/graphql',
       uri: 'http://localhost:3000/api/graphql',
     }),
   ]),
